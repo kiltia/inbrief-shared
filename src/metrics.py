6f5e0e8297cd21af1d722f02ae7413a46fa1b7be
Sergey Okunkov <makdaf007@yandex.ru>
2024-01-29 15:21:43 +0400

    Added optimal params for agglomerative clustering

--- src/metrics.py
+++ src/metrics.py
@@ -1,15 +1,10 @@
 import numpy as np
-from copy import deepcopy
+from utils import processing_noise
 from sklearn.metrics import silhouette_score, calinski_harabasz_score
 
 
 def silhouette(X, labels, metric="cityblock"):
-    labels_copy = deepcopy(labels)
-    cur = np.max(labels) + 1
-    for i in range(len(labels_copy)):
-        if labels_copy[i] == -1:
-            labels_copy[i] = cur
-            cur += 1
+    labels_copy = processing_noise(labels)
     try:
         return silhouette_score(X, labels_copy, metric=metric)
     except ValueError:
@@ -17,13 +12,16 @@ def silhouette(X, labels, metric="cityblock"):
 
 
 def calinski_harabasz(X, labels, **kwargs):
-    labels_copy = deepcopy(labels)
-    cur = np.max(labels) + 1
-    for i in range(len(labels_copy)):
-        if labels_copy[i] == -1:
-            labels_copy[i] = cur
-            cur += 1
+    labels_copy = processing_noise(labels)
     try:
         return calinski_harabasz_score(X, labels_copy)
+    except ValueError:
+        return np.nan
+    
+
+def weighted_metrics(X, labels, metric):
+    labels_copy = processing_noise(labels)
+    try:
+        return 0.01 * calinski_harabasz_score(X, labels_copy) + silhouette_score(X, labels_copy, metric=metric)
     except ValueError:
         return np.nan
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 from numpy.linalg import norm
 
 
@@ -10,3 +11,12 @@ def cos_sim(embedding1, embedding2):
 
 def get_class_score(embedding1, embedding2, scorer=cos_sim):
     return scorer(np.array(embedding1), np.array(embedding2))
+
+def processing_noise(labels):
+    labels_copy = deepcopy(labels)
+    cur = np.max(labels) + 1
+    for i in range(len(labels_copy)):
+        if labels_copy[i] == -1:
+            labels_copy[i] = cur
+            cur += 1
+    return labels_copy
