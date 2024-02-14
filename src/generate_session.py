05ebcb05db114f76d6fd7d8b2ca971ac0d8017ff
Nikita Rydanov <find.art.in.living@gmail.com>
2024-02-14 10:21:40 +0400

    Added system_version parameter

--- src/generate_session.py
+++ src/generate_session.py
@@ -6,5 +6,10 @@ from config import Credentials
 
 creds = Credentials()
 
-with TelegramClient("sessions/dev", creds.api_id, creds.api_hash) as client:
+with TelegramClient(
+    "sessions/dev",
+    creds.api_id,
+    creds.api_hash,
+    system_version="4.16.30-vxCUSTOM",
+) as client:
     client.session.save()
