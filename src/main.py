5636ad634777002dbb286b6c7f4d758072a59f41
Sergey Okunkov <makdaf007@yandex.ru>
2024-02-15 21:32:00 +0400

    Made some changes in db structure and delete db connect from linker service

--- src/main.py
+++ src/main.py
@@ -1,53 +1,17 @@
 import logging
-from contextlib import asynccontextmanager
 
 from asgi_correlation_id import CorrelationIdMiddleware
-from databases import Database
 from fastapi import FastAPI
 from matcher import Matcher
 
-from shared.db import PgRepository, create_db_string
-from shared.entities import Story, StorySource
-from shared.logger import configure_logging
 from shared.models import LinkingRequest
-from shared.resources import SharedResources
 from shared.routes import LinkerRoutes
-from shared.utils import SHARED_CONFIG_PATH
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    configure_logging()
-    await ctx.init_db()
-    yield
-    await ctx.dispose_db()
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.add_middleware(CorrelationIdMiddleware, validator=None)
 
 
 logger = logging.getLogger("app")
-
-
-class Context:
-    def __init__(self):
-        self.shared_resources = SharedResources(
-            f"{SHARED_CONFIG_PATH}/settings.json"
-        )
-        self._pg = Database(create_db_string(self.shared_resources.pg_creds))
-        self.story_repository = PgRepository(self._pg, Story)
-        self.story_post_repository = PgRepository(self._pg, StorySource)
-        self.ranker = None
-
-    async def init_db(self):
-        await self._pg.connect()
-
-    async def dispose_db(self):
-        await self._pg.disconnect()
-
-
-ctx = Context()
 
 
 @app.post(LinkerRoutes.GET_STORIES)
